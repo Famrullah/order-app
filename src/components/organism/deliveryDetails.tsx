9359da7acbd4f066fe96461c8d38f8dac29ec054
Faisal Amrullah <faisal.amrullah@kitabisa.com>
2023-07-17 03:24:42 +0700

    fix: fixing responsive header

--- src/components/organism/deliveryDetails.tsx
+++ src/components/organism/deliveryDetails.tsx
@@ -18,20 +18,20 @@ export default function DeliveryDetails() {
   return (
     <div className="md:w-3/4 xs:w-full">
       <div className="lg:border-r border-1 lg:border-[#FF8A00] md:border-r md:border-[#FF8A00] mr-4 min-h-[400px]">
-        <h3 className="text-[#FF8A00] text-4xl font-semibold">Delivery Details</h3> 
+        <h3 className="text-[#FF8A00] text-2xl md:text-4xl font-semibold">Delivery Details</h3> 
           {watchValue}
         <div className="py-5">
           <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="flex gap-x-8 mt-2 lg:max-w-[90%]">
-              <input {...register("exampleRequired", { required: true })} className={`${errors.exampleRequired ? 'outline-stone-600':''} border border-[#ccc] mt-2 p-4 w-[60%] rounded`} placeholder="Email"/>
-              <input {...register("exampleRequired", { required: true })} className={`${errors.exampleRequired ? 'outline-stone-600':''} border border-[#ccc] mt-2 p-4 w-[35%] rounded`} placeholder="Dropshipper Name"/>
+            <div className="md:flex gap-x-4 mt-2 w-full md:pr-5">
+              <input {...register("exampleRequired", { required: true })} className={`${errors.exampleRequired ? 'outline-stone-600':''} border border-[#ccc] mt-2 p-4 w-full md:w-[60%] rounded`} placeholder="Email"/>
+              <input {...register("exampleRequired", { required: true })} className={`${errors.exampleRequired ? 'outline-stone-600':''} border border-[#ccc] mt-2 p-4 w-full md:w-[40%] rounded`} placeholder="Dropshipper Name"/>
             </div>
-            <div className="flex gap-x-8 mt-2 lg:max-w-[90%]">
-              <input {...register("exampleRequired", { required: true })} className={`${errors.exampleRequired ? 'outline-stone-600':''} border border-[#ccc] mt-2 p-4 w-[60%] rounded`} placeholder="Phone Number"/>
-              <input {...register("exampleRequired", { required: true })} className={`${errors.exampleRequired ? 'outline-stone-600':''} border border-[#ccc] mt-2 p-4 w-[35%] rounded`} placeholder="Dropshipper Phone Number"/>
+            <div className="md:flex gap-x-4 mt-2 w-full md:pr-5">
+              <input {...register("exampleRequired", { required: true })} className={`${errors.exampleRequired ? 'outline-stone-600':''} border border-[#ccc] mt-2 p-4 w-full md:w-[60%] rounded`} placeholder="Phone Number"/>
+              <input {...register("exampleRequired", { required: true })} className={`${errors.exampleRequired ? 'outline-stone-600':''} border border-[#ccc] mt-2 p-4 w-full md:w-[40%] rounded`} placeholder="Dropshipper Phone Number"/>
             </div>
-            <div className="flex gap-x-8 mt-2 lg:max-w-[90%]">
-              <textarea {...register("exampleRequired", { required: true })} className={`${errors.exampleRequired ? 'outline-stone-600':''} border border-[#ccc] mt-2 p-4 w-[60%] rounded`} placeholder="Delivery Address" rows={4} cols={50}/>
+            <div className="flex gap-x-4 mt-2 w-full md:pr-5">
+              <textarea {...register("exampleRequired", { required: true })} className={`${errors.exampleRequired ? 'outline-stone-600':''} border border-[#ccc] mt-2 w-full p-4 md:w-[59%] rounded`} placeholder="Delivery Address" rows={4} cols={50}/>
             </div>
             
             {/* errors will return when field validation fails  */}
