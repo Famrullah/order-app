ec97dcbafa6ac2079f225405a574f02db39809a2
Faisal Amrullah <faisal.amrullah@kitabisa.com>
2023-07-17 02:56:05 +0700

    fix: fixing responsive header

--- src/components/molekul/header.tsx
+++ src/components/molekul/header.tsx
@@ -3,10 +3,8 @@ import Wizard from "../atom/wizard"
 
 export default function Header() {
   return (
-    <div className="">
-      <div className="m-auto bg-[#FFFAE6] md:w-3/4 xl:w-1/2 justify-center flex relative -top-[24px] p-5 rounded-[30px] ">
-        <Wizard/> 
-      </div>
+    <div className="m-auto bg-[#FFFAE6] md:w-3/4 xl:w-1/2 justify-center flex relative -top-[24px] p-5 rounded-[30px] ">
+      <Wizard/> 
     </div>
   )
 }
--- src/container/dashboardContainer/index.tsx
+++ src/container/dashboardContainer/index.tsx
@@ -2,11 +2,11 @@
 import DeliveryDetails from "@/components/organism/deliveryDetails"
 import Summary from "@/components/molekul/summary"
 
-export default function DashboardContainer() {
+export default function Home() {
   const data =[{ name: 'Cost of goods', price: 500000 },{ name: 'Dropshiping fee', price: 5900 }]
   return (
     <>
-      <div className="md:flex flex-wrap mt-5 pb-5">
+      <div className="mt-5 pb-5 md:flex flex-wrap">
         <DeliveryDetails/>
         <Summary 
           total={505900}
